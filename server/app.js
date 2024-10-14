33c0f2e15191b2e5e6fc849f3c19da15aa146a76
Dimentrius <lavrentevd579@gmail.com>
2024-10-14 18:41:04 +0300

    добавлены мидлвеер для роутеров и фукция синхронизации с бд при прослушке, а так же подлкючен cors

--- server/app.js
+++ server/app.js
@@ -1,7 +1,35 @@
-const express = require('express');
+const express = require("express");
+const path = require('path');
+const cors = require("cors")
+const db = require('./db.js');
+const router = require('./appRoutes.js');
+const models = require('./models.js');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(cors())       
+// const urlencodedParser = express.urlencoded({extended: false});
 
-app.listen(PORT);
+app.use('/articles', router);
+app.use('/article', router);
+
+app.listen(PORT, async () => {
+    try {
+        await db.connect.authenticate();
+        console.log("база данных подключена")
+        await db.connect.sync();
+        console.log("база данных синхронизирована")
+    }
+    catch (e) {
+        console.log(`ошибка^ ${e}`);
+    }
+})
+
+
+
+
+
+
+
