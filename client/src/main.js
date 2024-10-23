9aace9f695fb3e8ddc77db4920ac8bb8a6bf5065
Dimentrius <lavrentevd579@gmail.com>
2024-10-23 07:04:27 +0300

    установка нужных пакетов

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,24 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import store from './vueX/store';
 const app = createApp(App)
 
-app.use(router)
 
-app.mount('#app')
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+
+const vuetify = createVuetify({
+      
+    components,
+    directives,
+  })
+
+
+  
+app.use(vuetify).use(router).use(store).mount('#app')
+
