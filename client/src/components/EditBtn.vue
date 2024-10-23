9cd0351a3e8841c109da8fb43607b9891794bfc1
Dimentrius <lavrentevd579@gmail.com>
2024-10-23 07:25:20 +0300

    кнопка с drop-down передающая нунжное событие для взаимодействия со статьёй

---
+++ client/src/components/EditBtn.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-btn
+      color="primary">
+      Изменить
+      <v-menu activator="parent">
+        <v-list>
+        <v-list-item>
+          <v-btn @click="$emit('edit', 'editing')"> Редактировать </v-btn>
+        </v-list-item>
+        <v-list-item>
+          <v-btn @click="$emit('edit', 'delete')"> Удалить </v-btn>
+        </v-list-item>
+      </v-list>
+      </v-menu>
+    </v-btn>
+</template>
+<script>
+    export default{
+        emits: ['edit']
+    }
+</script>
