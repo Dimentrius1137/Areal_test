c97764c69a5224ca0aa52dd169d99c1d631250e4
Dimentrius <lavrentevd579@gmail.com>
2024-10-23 07:35:15 +0300

    удаление ненужных компонентов

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,85 +1,40 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Header></Header>
+  <div class="content" :class="{ darkContent: darkTheme }">
+    <RouterView v-cloak></RouterView>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<script>
+import { mapState } from 'vuex';
+import { RouterView } from 'vue-router'
+import Header from '@/components/Header.vue'
+export default{
+  components: { Header },
+  data(){
+    return{
+    }
+  },
+  computed: {
+    ...mapState('theme', ['darkTheme']),
+  }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
+<style scoped lang="scss">
+    [v-cloak]{
+      display: none;
+    }
+  .content{
+    width: 100%;
+    background-color: #E0E0E0;
+    min-height: calc(100% - 60px);
+    height: auto;
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
+  .darkContent{
+    background-color:rgba(18,18,18, 0.9);
+    color: white;
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
